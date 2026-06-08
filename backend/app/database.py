6e6a35016bbb32500a35d43a8b9f549ca31f84ac
sesettikalyan <sesettikalyan@gmail.com>
2026-06-08 19:51:09 +0530

    feat: implement database configuration and user model with dependency injection setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker , declarative_base
 
 load_dotenv()
 
@@ -17,3 +17,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
